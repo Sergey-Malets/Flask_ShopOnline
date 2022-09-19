736202eb26bda3faee9a0639b1bd887f383a61c4
Malets_Sergey <maletssergey@gmail.com>
2022-09-19 13:04:50 +0300

    hw.1

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello'
+    return render_template('index.html', title='main')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='about')
+
+@app.route('/new_page')
+def new_page():
+    return render_template('new_page.html', title='new page')
 
 
 if __name__ == '__main__':
