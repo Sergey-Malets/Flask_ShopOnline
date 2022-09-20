6d76e2659019f6fd2cea513244f9bd8f572a1702
Malets_Sergey <maletssergey@gmail.com>
2022-09-20 12:58:48 +0300

    hw.3 class Item

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_image_alchemy.storages import S3Storage
+from flask_image_alchemy.fields import StdImageField
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db = SQLAlchemy(app)
+s3_storage = S3Storage()
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    price = 0
+    isActive = 0
+    image = db.Column(
+        StdImageField(
+            storage=s3_storage,
+            variations={
+                'thumbnail': {"width": 100, "height": 100, "crop": True}
+            }
+        )
+    )
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route('/')
 def index():
